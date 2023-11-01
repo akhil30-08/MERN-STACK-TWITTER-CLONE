9d41ad858d733a8c73d3c0f539f15f42f1318c5c
Akhil <akhilm.jmu@yahoo.com>
2023-11-01 20:37:07 +0530

    cors fixed

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const path = require('path');
 const dotenv = require('dotenv');
 dotenv.config();
 
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../client/dist')));
