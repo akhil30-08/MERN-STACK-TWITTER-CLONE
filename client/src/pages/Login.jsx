f222923bb3b15f898d6b3a2e375225681db039c3
Akhil <akhilm.jmu@yahoo.com>
2024-11-02 17:54:13 +0530

    bug: utils file

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -12,105 +12,117 @@ import 'react-toastify/dist/ReactToastify.css';
 import LoadingSpinner from '../components/Spinner';
 
 function Login() {
-  //to handle state of Email and Password
-  const [Username, setUsername] = useState('');
-  const [Password, setPassword] = useState('');
-  const [loading, setLoading] = useState(false);
+   //to handle state of Email and Password
+   const [Username, setUsername] = useState('');
+   const [Password, setPassword] = useState('');
+   const [loading, setLoading] = useState(false);
 
-  //initialise usenavigate and usedispatch
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
+   //initialise usenavigate and usedispatch
+   const navigate = useNavigate();
+   const dispatch = useDispatch();
 
-  //toast functions
-  const notify = () =>
-    toast.success('Logged In Successfully!', {
-      position: 'top-right',
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-      theme: 'light',
-    });
-
-  const notifyError = () =>
-    toast.error('Invalid Credentials !', {
-      position: 'top-right',
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-      theme: 'light',
-    });
-
-  //function to call LogIn API
-  const handleLogin = async (e) => {
-    e.preventDefault();
-    dispatch(loginStart());
-    setLoading(true);
-    try {
-      const request = await axios.post(`${Base_URL}/api/auth/login`, {
-        Username,
-        Password,
+   //toast functions
+   const notify = () =>
+      toast.success('Logged In Successfully!', {
+         position: 'top-right',
+         hideProgressBar: false,
+         closeOnClick: true,
+         pauseOnHover: true,
+         draggable: true,
+         progress: undefined,
+         theme: 'light',
       });
-      dispatch(loginSuccess(request.data.user));
 
-      //store the token, username and fullname and in local storage
-      localStorage.setItem('token', request.data.token);
-      localStorage.setItem('username', request.data.user.Username);
-      localStorage.setItem('full_name', request.data.user.Name);
-      localStorage.setItem('_id', request.data.user._id);
-      notify();
-      navigate('/home');
-    } catch (error) {
-      dispatch(loginFailed());
-      notifyError();
-      setLoading(false);
-    }
-  };
+   const notifyError = () =>
+      toast.error('Invalid Credentials !', {
+         position: 'top-right',
+         hideProgressBar: false,
+         closeOnClick: true,
+         pauseOnHover: true,
+         draggable: true,
+         progress: undefined,
+         theme: 'light',
+      });
 
-  return (
-    <>
-      <Helmet>
-        <title>Login</title>
-      </Helmet>
+   //function to call LogIn API
+   const handleLogin = async (e) => {
+      e.preventDefault();
+      dispatch(loginStart());
+      setLoading(true);
+      try {
+         const request = await axios.post(`${Base_URL}/api/auth/login`, {
+            Username,
+            Password,
+         });
+         dispatch(loginSuccess(request.data.user));
 
-      <div>
-        <h4 className='ms-4' id='login-text'>
-          Log In
-        </h4>
-        <form className='mt-2 px-4' onSubmit={handleLogin}>
-          <input
-            type='text'
-            className='form-control mt-3'
-            placeholder='Username'
-            value={Username}
-            onChange={(e) => setUsername(e.target.value)}
-          ></input>
-          <input
-            type='password'
-            className='form-control mt-3'
-            id='exampleInputPassword1'
-            placeholder='Password'
-            value={Password}
-            onChange={(e) => setPassword(e.target.value)}
-          ></input>
-          <button type='submit' className='mt-3 px-3 btn btn-dark'>
-            Login
-          </button>
+         //store the token, username and fullname and in local storage
+         localStorage.setItem('token', request.data.token);
+         localStorage.setItem('username', request.data.user.Username);
+         localStorage.setItem('full_name', request.data.user.Name);
+         localStorage.setItem('_id', request.data.user._id);
+         notify();
+         navigate('/home');
+      } catch (error) {
+         dispatch(loginFailed());
+         notifyError();
+         setLoading(false);
+      }
+   };
 
-          {loading && <LoadingSpinner />}
-        </form>
-        <p className='mt-4 ms-4 '>
-          <span className='text-muted'>Don't have an account?</span>
-          <Link to='/signup' className='text-primary'>
-            Register Here
-          </Link>
-        </p>
-      </div>
-    </>
-  );
+   return (
+      <>
+         <Helmet>
+            <title>Login</title>
+         </Helmet>
+
+         <div>
+            <h4
+               className='ms-4'
+               id='login-text'
+            >
+               Log In
+            </h4>
+            <form
+               className='mt-2 px-4'
+               onSubmit={handleLogin}
+            >
+               <input
+                  type='text'
+                  className='form-control mt-3'
+                  placeholder='Username'
+                  value={Username}
+                  onChange={(e) => setUsername(e.target.value)}
+               ></input>
+               <input
+                  type='password'
+                  className='form-control mt-3'
+                  id='exampleInputPassword1'
+                  placeholder='Password'
+                  value={Password}
+                  onChange={(e) => setPassword(e.target.value)}
+               ></input>
+               <button
+                  type='submit'
+                  className='mt-3 px-3 btn btn-dark'
+               >
+                  Login
+               </button>
+
+               {loading && <LoadingSpinner />}
+            </form>
+            <p className='mt-4 ms-4 '>
+               <span className='text-muted'>Don&apos;t have an account?</span>
+               <Link
+                  to='/signup'
+                  className='text-primary'
+               >
+                  Register Here
+               </Link>
+            </p>
+         </div>
+      </>
+   );
 }
 
 export default Login;
--- client/src/utils.js
+++ client/src/utils.js
@@ -1,3 +1,3 @@
-const Base_URL = process.env.NODE_ENV === 'development' ? 'https://mern-stack-twitter-clone-backend.vercel.app' : '';
+const Base_URL = process.env.NODE_ENV !== 'development' ? 'https://mern-stack-twitter-clone-backend.vercel.app' : '';
 
 export default Base_URL;
